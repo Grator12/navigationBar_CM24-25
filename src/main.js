fc766a7f91b25ebe965ef1da485673520016fcc7
Grator12 <timaintula@gmail.com>
2024-11-18 01:50:57 +0300

    add interactive buttons and com removed

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import("./main.scss");
+
+document.querySelectorAll(".navigation-list__button").forEach((el) => {
+    el.addEventListener("click", (e) => {
+        document.querySelectorAll(".active").forEach((item) => {
+            item.classList.remove("active");
+        })
+        el.classList.add('active');
+    })
+
+})
+
